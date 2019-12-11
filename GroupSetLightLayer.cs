c4bf31c8ee30fb130e47850d623a0776e1069044
Arvind Neelakantan <arvind@unity3d.com>
2019-12-11 17:57:58 +0530

    (Arvind) Group Setting LightLayers in HDRP

---
+++ GroupSetLightLayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+
+[ExecuteInEditMode]
+public class GroupSetLightLayer : MonoBehaviour
+{
+    public LightLayerEnum LightLayer;
+
+    private MeshRenderer[] _meshRenderers;
+    
+    public void SetLightLayerToChildren()
+    {
+        _meshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        foreach (MeshRenderer meshRenderer in _meshRenderers)
+        {
+            meshRenderer.renderingLayerMask = (uint)LightLayer;
+        }
+    }
+    
+}
---
+++ GroupSetLightLayerEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GroupSetLightLayer))]
+[CanEditMultipleObjects]
+public class GroupSetLightLayerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GroupSetLightLayer groupSetLightLayer = (GroupSetLightLayer) target;
+
+        if (GUILayout.Button("Set LightLayer To Children"))
+        {
+            groupSetLightLayer.SetLightLayerToChildren();
+        }
+    }
+}
